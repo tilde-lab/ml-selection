8127e437fae63d7c69365cab918f9f891cc7aaf8
alinzh <siberiangodness@mail.ru>
2024-04-15 20:51:58 +0000

    Add tests for datasets

---
+++ tests/test_datasets/test_crystal_graph_dataset.py
@@ -0,0 +1,49 @@
+"""
+Test for build graph by CrystalGraphDataset
+"""
+
+import unittest
+
+import pandas as pd
+from torch_geometric.data import Data
+from torch_geometric.loader import DataLoader
+from torch import Tensor
+
+from datasets.crystal_graph_dataset import CrystalGraphDataset
+
+
+class TestRequestMPDS(unittest.TestCase):
+    def setUp(self):
+        # DataFrame with columns:
+        # ['phase_id', 'Formula', 'Seebeck coefficient', 'cell_abc', 'sg_n', 'basis_noneq', 'els_noneq']
+        self.dfrm = self.data_csv = pd.read_csv(
+            "/root/projects/ml-selection/data/processed_data/total.csv"
+        )
+        self.dataset = CrystalGraphDataset()
+
+    def test_build_graph(self):
+        graph, seebeck = self.dataset.build_graph(self.dfrm.values.tolist()[0])
+        self.assertEqual(type(seebeck), float, "incorrect type of Seebeck")
+        self.assertEqual(type(graph), Data, "incorrect type of graph")
+        self.assertEqual(
+            len(graph.x) * len(graph.x) - len(graph.x),
+            len(graph.edge_index[0]),
+            "incorrectly defined edges between atoms",
+        )
+        self.assertEqual(len(graph.x[0]), 4, "incorrect number of features for nodes")
+        self.assertEqual(
+            len(graph.edge_index[0]),
+            len(graph.edge_attr),
+            "incorrect number of features for edges",
+        )
+
+    def test_get_item(self):
+        dataloader = DataLoader(
+            self.dataset, batch_size=1, shuffle=False, num_workers=0
+        )
+        for batch in dataloader:
+            self.assertEqual(type(batch[0].x), Tensor, "incorrect type")
+            self.assertEqual(
+                len(batch[0].x[0]), 4, "incorrect number of features for nodes"
+            )
+            break
---
+++ tests/test_datasets/test_vectors_graph_dataset.py
@@ -0,0 +1,35 @@
+"""
+Test for build graph by CrystalGraphVectorsDataset
+"""
+
+import unittest
+
+import pandas as pd
+from torch_geometric.data import Data
+
+from datasets.vectors_graph_dataset import CrystalGraphVectorsDataset
+
+
+class TestRequestMPDS(unittest.TestCase):
+    def setUp(self):
+        self.total = pd.read_csv(
+            "/root/projects/ml-selection/data/processed_data/under_str.csv",
+        )
+        self.seebeck = pd.read_csv(
+            "/root/projects/ml-selection/data/processed_data/under_seeb.csv",
+        )
+        self.data = pd.concat(
+            [self.seebeck["Seebeck coefficient"], self.total], axis=1
+        ).values.tolist()
+        self.dataset = CrystalGraphVectorsDataset()
+
+    def test_build_graph(self):
+        graph = self.dataset.build_graph(self.total.values.tolist()[0])
+
+        self.assertEqual(type(graph), Data, "incorrect type of graph")
+        self.assertEqual(
+            len(graph.x) * len(graph.x) - len(graph.x),
+            len(graph.edge_index[0]),
+            "incorrectly defined edges between atoms",
+        )
+        self.assertEqual(len(graph.x[0]), 2, "incorrect number of features for nodes")
