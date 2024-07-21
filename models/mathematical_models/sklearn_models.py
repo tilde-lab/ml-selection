097fc59d5db9f5d356fb14b37f7ab63986000555
alinzh <siberiangodness@mail.ru>
2024-07-21 20:28:24 +0000

    Format changes, update parms for RandomizedSearchCV

--- models/mathematical_models/sklearn_models.py
+++ models/mathematical_models/sklearn_models.py
@@ -1,6 +1,7 @@
 """
-Ml-models
+Ml-models from sklearn with tuning hyperparameters by RandomizedSearchCV.
 """
+
 import torch
 import numpy as np
 import polars as pl
@@ -23,6 +24,7 @@ from sklearn.metrics import explained_variance_score
 
 
 def main(just_mp: bool = False):
+    """Run pipeline"""
     with open("/root/projects/ml-selection/configs/config.yaml", "r") as yamlfile:
         yaml_f = yaml.load(yamlfile, Loader=yaml.FullLoader)
         raw_mpds = yaml_f["raw_mpds"]
@@ -44,14 +46,15 @@ def main(just_mp: bool = False):
             poly_path[i] = poly_path[i].replace(".json", "_mp.parquet")
 
     for cnt, f in enumerate([poly_features, poly_temperature_features]):
-        print(f'\n\nSTART with descriptor: {f}\n')
+        print(f"\n\nSTART with descriptor: {f}\n")
         if not just_mp:
             run_ml_models(poly_path, raw_mpds + "median_seebeck.parquet", f, cnt)
         else:
             run_ml_models(poly_path, raw_mpds + "mp_seebeck.parquet", f, cnt)
 
 
-def compute_metrics(y_pred, y_true) -> tuple:
+def compute_metrics(y_pred: torch.Tensor, y_true: torch.Tensor) -> tuple:
+    """Compute R2, MAE, EVS, Theil's U metrics"""
     mae, r2 = MeanAbsoluteError(), R2Score()
 
     mae.update(y_pred, y_true)
@@ -66,6 +69,7 @@ def compute_metrics(y_pred, y_true) -> tuple:
 
 
 def load_data(poly_path: str, seebeck_path: str) -> pd.DataFrame:
+    """Load data from '.parquet' files"""
     # Crystal in vectors format
     poly = pl.read_parquet(poly_path)
     seebeck = pl.read_parquet(seebeck_path)
@@ -78,25 +82,30 @@ def load_data(poly_path: str, seebeck_path: str) -> pd.DataFrame:
 
 
 def make_descriptors(data: pd.DataFrame, f: list, i: int, is_temp: bool):
-    poly_elements_df = pd.DataFrame(data['poly_elements'].tolist())
+    """Create 6 different type of descriptor"""
+    poly_elements_df = pd.DataFrame(data["poly_elements"].tolist())
     # there is no need to have same dim - take a number instead of an array
-    poly_type_df = pd.DataFrame([[row[0]] for row in data['poly_type'].values.tolist()])
-    y = data['Seebeck coefficient']
+    poly_type_df = pd.DataFrame([[row[0]] for row in data["poly_type"].values.tolist()])
+    y = data["Seebeck coefficient"]
 
     if is_temp:
-        temperature = data['temperature']
+        temperature = data["temperature"]
         if len(f[i]) == 3:
             x = pd.concat([poly_elements_df, poly_type_df, temperature], axis=1)
         else:
             # take a number instead of an array too
-            poly_v = pd.DataFrame([[row[0]] for row in data['poly_vertex'].values.tolist()])
+            poly_v = pd.DataFrame(
+                [[row[0]] for row in data["poly_vertex"].values.tolist()]
+            )
             x = pd.concat([poly_elements_df, poly_type_df, poly_v, temperature], axis=1)
     else:
         if len(f[i]) == 2:
             x = pd.concat([poly_elements_df, poly_type_df], axis=1)
         else:
             # take a number instead of an array too
-            poly_v = pd.DataFrame([[row[0]] for row in data['poly_vertex'].values.tolist()])
+            poly_v = pd.DataFrame(
+                [[row[0]] for row in data["poly_vertex"].values.tolist()]
+            )
             x = pd.concat([poly_elements_df, poly_type_df, poly_v], axis=1)
 
     train_size = int(0.9 * len(data))
@@ -106,9 +115,15 @@ def make_descriptors(data: pd.DataFrame, f: list, i: int, is_temp: bool):
     return train_x, train_y, test_x, test_y
 
 
-def run_ml_models(poly_paths: list, seebeck_path: str, f: list, is_temp: Union[bool, int], n_iter: int = 1) -> None:
+def run_ml_models(
+    poly_paths: list,
+    seebeck_path: str,
+    f: list,
+    is_temp: Union[bool, int],
+    n_iter: int = 100,
+) -> None:
     for i, poly in enumerate(poly_paths):
-        print(f'File: {poly}')
+        print(f"File: {poly}")
         data = load_data(poly, seebeck_path)
         train_x, train_y, test_x, test_y = make_descriptors(data, f, i, is_temp)
 
@@ -120,21 +135,21 @@ def run_ml_models(poly_paths: list, seebeck_path: str, f: list, is_temp: Union[b
 
 def run_linear_regression(X_train, y_train, X_test, y_test, n_iter):
     ridge = Ridge()
-    ridge_param_distributions = {
-        'alpha': np.random.uniform(0.00001, 10.0, size=10000)
-    }
+    ridge_param_distributions = {"alpha": np.random.uniform(0.0000001, 100.0, size=100000)}
     ridge_search = RandomizedSearchCV(
         estimator=ridge,
         param_distributions=ridge_param_distributions,
         n_iter=n_iter,
-        scoring='r2',
+        scoring="r2",
         cv=5,
         verbose=1,
-        random_state=42
+        random_state=42,
     )
     ridge_search.fit(X_train, y_train)
     pred = ridge_search.predict(X_test)
-    r2, mae, evs, tur = compute_metrics(torch.from_numpy(pred), torch.tensor(y_test.values))
+    r2, mae, evs, tur = compute_metrics(
+        torch.from_numpy(pred), torch.tensor(y_test.values)
+    )
 
     print("Best parms for Ridge:", ridge_search.best_params_)
     print(f"r2: {r2}, mae: {mae}, evs: {evs}, tur: {tur}")
@@ -143,25 +158,27 @@ def run_linear_regression(X_train, y_train, X_test, y_test, n_iter):
 def run_boosted_trees(X_train, y_train, X_test, y_test, n_iter):
     gbm = GradientBoostingRegressor()
     gbm_param_distributions = {
-        'n_estimators': randint(1, 10),
-        'learning_rate': np.random.uniform(0.000001, 0.5, size=100000),
-        'max_depth': randint(3, 10),
-        'min_samples_leaf': randint(1, 10),
-        'min_samples_split': randint(1, 10)
+        "n_estimators": randint(1, 100),
+        "learning_rate": np.random.uniform(0.000001, 0.5, size=100000),
+        "max_depth": randint(3, 100),
+        "min_samples_leaf": randint(1, 100),
+        "min_samples_split": randint(1, 100),
     }
 
     gbm_search = RandomizedSearchCV(
         estimator=gbm,
         param_distributions=gbm_param_distributions,
         n_iter=n_iter,
-        scoring='r2',
+        scoring="r2",
         cv=5,
         verbose=1,
-        random_state=42
+        random_state=42,
     )
     gbm_search.fit(X_train, y_train)
     pred = gbm_search.predict(X_test)
-    r2, mae, evs, tur = compute_metrics(torch.from_numpy(pred), torch.tensor(y_test.values))
+    r2, mae, evs, tur = compute_metrics(
+        torch.from_numpy(pred), torch.tensor(y_test.values)
+    )
 
     print("Best parms for Gradient Boosting:", gbm_search.best_params_)
     print(f"r2: {r2}, mae: {mae}, evs: {evs}, tur: {tur}")
@@ -170,24 +187,26 @@ def run_boosted_trees(X_train, y_train, X_test, y_test, n_iter):
 def run_decision_tree(X_train, y_train, X_test, y_test, n_iter):
     decision_tree = DecisionTreeRegressor()
     param_distributions = {
-        'max_depth': randint(1, 6),
-        'min_samples_split': randint(1, 10),
-        'min_samples_leaf': randint(1, 10)
+        "max_depth": randint(1, 200),
+        "min_samples_split": randint(1, 100),
+        "min_samples_leaf": randint(1, 100),
     }
 
     random_search = RandomizedSearchCV(
         estimator=decision_tree,
         param_distributions=param_distributions,
         n_iter=n_iter,
-        scoring='r2',
+        scoring="r2",
         cv=5,
         verbose=1,
-        random_state=42
+        random_state=42,
     )
 
     random_search.fit(X_train, y_train)
     pred = random_search.predict(X_test)
-    r2, mae, evs, tur = compute_metrics(torch.from_numpy(pred), torch.tensor(y_test.values))
+    r2, mae, evs, tur = compute_metrics(
+        torch.from_numpy(pred), torch.tensor(y_test.values)
+    )
 
     print("Best parms for DecisionTree:", random_search.best_params_)
     print(f"r2: {r2}, mae: {mae}, evs: {evs}, tur: {tur}")
@@ -196,24 +215,26 @@ def run_decision_tree(X_train, y_train, X_test, y_test, n_iter):
 def run_random_forest(X_train, y_train, X_test, y_test, n_iter):
     rf = RandomForestRegressor()
     rf_param_distributions = {
-        'n_estimators': randint(1, 10),
-        'max_depth': randint(1, 10),
-        'min_samples_split': randint(2, 10)
+        "n_estimators": randint(1, 100),
+        "max_depth": randint(1, 100),
+        "min_samples_split": randint(1, 100),
     }
 
     rf_search = RandomizedSearchCV(
         estimator=rf,
         param_distributions=rf_param_distributions,
         n_iter=n_iter,
-        scoring='r2',
+        scoring="r2",
         cv=5,
         verbose=1,
-        random_state=42
+        random_state=42,
     )
     rf_search.fit(X_train, y_train)
 
     pred = rf_search.predict(X_test)
-    r2, mae, evs, tur = compute_metrics(torch.from_numpy(pred), torch.tensor(y_test.values))
+    r2, mae, evs, tur = compute_metrics(
+        torch.from_numpy(pred), torch.tensor(y_test.values)
+    )
 
     print("Best parms for Random Forest:", rf_search.best_params_)
     print(f"r2: {r2}, mae: {mae}, evs: {evs}, tur: {tur}")
