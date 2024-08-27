e296e87245fe324106617425d6b65f9cc3ce37ee
alinzh <siberiangodness@mail.ru>
2024-08-27 16:14:17 +0000

    Add finding of polyhedra from any structure

--- data_massage/polyhedra/get_poly_from_ase.py
+++ data_massage/polyhedra/get_poly_from_ase.py
@@ -1,130 +1,133 @@
-import numpy as np
-from scipy.spatial import distance
-from operator import itemgetter
-from ase.data import chemical_symbols
-from data.mendeleev_table import get_periodic_number
 from metis_backend.metis_backend.datasources.fmt import detect_format
 from metis_backend.metis_backend.structures.cif_utils import cif_to_ase
+from metis_backend.metis_backend.structures.struct_utils import poscar_to_ase, optimade_to_ase
+
+from ase import Atoms
+from ase.neighborlist import NeighborList
+
+import numpy as np
+import polars as pl
 
 
-def find_metal(atom=None, coord=None):
+def extract_poly(atom=None, coord=None, cell=None, cutoff=3.0):
     """
-    Count the number of metal center atom in complex.
-
-    Parameters
-    ----------
-    atom : list, None
-        Full atomic labels of complex.
-    coord : array_like, None
-        Full atomic coordinates of complex.
-
-    Returns
-    -------
-    total_metal : int
-        The total number of center atom.
-    atom_metal : list
-        Atomic labels of center atom.
-    coord_metal : ndarray
-        Atomic coordinates of  center atom.
-    """
-    if atom is None or coord is None:
-        raise TypeError("find_metal needs two arguments: atom, coord")
-
-    total_metal = 0
-    atom_metal = []
-    coord_metal = []
-
-    for i in range(len(atom)):
-        number = get_periodic_number(atom[i])
-
-        if 21 <= number <= 30 or \
-                39 <= number <= 48 or \
-                57 <= number <= 80 or \
-                89 <= number <= 109:
-
-            total_metal += 1
-            atom_metal.append(atom[i])
-            coord_metal.append(coord[i])
-
-    coord_metal = np.asarray(coord_metal, dtype=np.float64)
-
-    return total_metal, atom_metal, coord_metal
-
-
-def extract_poly(atom=None, coord=None, n_center=1, cutoff_ligand=2.8, number_of_atoms=1):
-    """
-    Search the polyhedron structure in complex and return atoms and coordinates.
+    Extract all polyhedrons in the structure by finding neighboring atoms within the cutoff distance.
 
     Parameters
     ----------
     atom : list
-        Full atomic labels of complex.
-    coord : array_like
-        Full atomic coordinates of complex.
-    n_center : int
-        Number of metal atom that will be taken as center atom for
-        finding atomic coordinates of polyhedron structure of interest.
-        Default value is 1.
-    cutoff_ligand : float, optional
-        Cutoff distance for screening ligand bond.
-        Default value is 2.8.
-    number_of_atoms : int, optional
-        Number of atoms in polyhedron
+        Full atomic labels
+    coord : array
+        Full atomic coordinates
+    cell : array
+        Lattice vectors of the unit cell
+    cutoff : float, optional
+        Cutoff distance for finding neighboring atoms.
+        Default value is 3.0
 
     Returns
     -------
-    atom_octa : list
-        Atomic labels of polyhedron structure.
-    coord_polyhedron : ndarray
-        Atomic coordinates of polyhedron structure.
+    polyhedrons : list of dict
+        List of dictionaries containing atomic labels and coordinates of polyhedrons
     """
-    if atom is None or coord is None:
-        raise TypeError("needs two arguments: atom and coord")
+    if atom is None or coord is None or cell is None:
+        raise TypeError("extract_poly needs three arguments: atom, coord, and cell")
 
-    # Count the number of metal center atom
-    total_metal, atom_metal, coord_metal = find_metal(atom, coord)
+    atoms = Atoms(symbols=atom, positions=coord, cell=cell, pbc=True)
 
-    if n_center <= 0:
-        raise ValueError("index of metal must be positive integer")
+    # atomic cut-off radius
+    cutoffs = [cutoff] * len(atoms)
 
-    elif n_center > total_metal:
-        raise ValueError("user-defined index of metal is greater than "
-                         "the total metal in complex.")
+    # make list with all neighbors in current cut-off
+    nl = NeighborList(cutoffs, self_interaction=False, bothways=True)
+    nl.update(atoms)
 
-    metal_index = n_center - 1
-    dist_list = []
+    polyhedrons = []
 
-    for i in range(len(list(atom))):
-        dist = distance.euclidean(coord_metal[metal_index], coord[i])
-        if dist <= cutoff_ligand:
-            dist_list.append([atom[i], coord[i], dist])
+    for i, center_atom in enumerate(atom):
+        # offsets - shifts of neighboring atoms if they are outside cell
+        indices, offsets = nl.get_neighbors(i)
 
-    # sort list of tuples by distance in ascending order
-    dist_list.sort(key=itemgetter(2))
+        # for each neighbor found, its coordinates and distance to the central atom
+        neighbor_coords = atoms.positions[indices] + np.dot(offsets, atoms.get_cell())
+        distances = np.linalg.norm(neighbor_coords - atoms.positions[i], axis=1)
 
-    # Keep only first n atoms
-    dist_list = dist_list[:number_of_atoms]
-    atom_polyhedron, coord_polyhedron, dist = zip(*dist_list)
+        if len(indices) > 0:  # if found neighbors
+            polyhedron = {
+                "center_atom": center_atom,
+                "center_coord": atoms.positions[i],
+                "atoms": [center_atom] + [atom[j] for j in indices],
+                "coords": [atoms.positions[i].tolist()] + neighbor_coords.tolist(),
+                "distances": [0.0] + distances.tolist()  # 0.0 for central atom
+            }
+            polyhedrons.append(polyhedron)
 
-    atom_polyhedron = list(atom_polyhedron)
-    coord_polyhedron = np.asarray(coord_polyhedron, dtype=np.float64)
-
-    return atom_polyhedron, coord_polyhedron
-
-
-def get_poly_info(ase_obj):
-    atoms_poly, coord_poly = extract_poly(
-        [chemical_symbols[i] for i in ase_obj.numbers], ase_obj.positions, number_of_atoms=16
-    )
-    print(atoms_poly)
+    return polyhedrons
 
 
-def get_poly_type():
-    NotImplemented
+def get_polyhedrons(path_structures: str, path_to_save: str, cutoff=3.0):
+    poly_store = []
+
+    structures = open(path_structures).read()
+    fmt = detect_format(structures)
+
+    if fmt == "cif":
+        atoms, error = cif_to_ase(structures)
+        if error:
+            raise RuntimeError(error)
+    elif fmt == "poscar":
+        atoms, error = poscar_to_ase(structures)
+        if error:
+            raise RuntimeError(error)
+
+    elif fmt == "optimade":
+        atoms, error = optimade_to_ase(structures, skip_disorder=True)
+        if error:
+            raise RuntimeError(error)
+
+    # if the data is received from mpds
+    else:
+        structures = pl.read_json(path_structures)
+        for structure in structures.rows():
+            atoms = Atoms(
+                symbols=structure[4],
+                positions=structure[3],
+                cell=structure[1]
+            )
+
+            # found all available polyhedrons for that structure
+            polyhedrons = extract_poly(
+                atom=[symbol for symbol in atoms.get_chemical_symbols()],
+                coord=atoms.get_positions(),
+                cell=atoms.get_cell(),
+                cutoff=cutoff
+            )
+
+            for poly in polyhedrons:
+                poly_store.append(
+                    [structure[0], structure[5], poly["atoms"], poly["coords"], poly["distances"]]
+                )
+
+    if fmt in ["cif", "poscar", "optimade"]:
+        # found all available polyhedrons for that structure
+        polyhedrons = extract_poly(
+            atom=[symbol for symbol in atoms.get_chemical_symbols()],
+            coord=atoms.get_positions(),
+            cell=atoms.get_cell(),
+            cutoff=cutoff
+        )
+
+        for poly in polyhedrons:
+            poly_store.append(
+                [atoms[0], atoms[5], poly["atoms"], poly["coords"], poly["distances"]]
+            )
+
+    res = pl.DataFrame(poly_store, schema=["phase_id", "entry", "atoms", "coords", "distances"]).write_json(path_to_save)
+    print(res)
 
 
-input_file = '/root/projects/ml-selection/input'
-structure = open(input_file).read()
-fmt = detect_format(structure)
-ase_obj, _ = cif_to_ase(structure)
-get_poly_info(ase_obj)
+if __name__ == "__main__":
+    path = 'ml-selection/data/raw_mpds/rep_structures_mpds_seeb.json'
+    path_to_save = 'ml-selection/data/poly/poly_4_seeb.json'
+
+    get_polyhedrons(path, path_to_save)
