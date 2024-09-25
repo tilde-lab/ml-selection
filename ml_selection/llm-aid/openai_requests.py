5ff5e88d653ce61ff5f4613f369b06e19ff4a18d
alinzh <siberiangodness@mail.ru>
2024-09-25 15:35:40 +0000

    Add module for request by openai

---
+++ ml_selection/llm-aid/openai_requests.py
@@ -0,0 +1,18 @@
+
+from openai import OpenAI
+
+print('API KEY:')
+client = OpenAI(
+    api_key=str(input())
+)
+
+models = client.models.list()
+
+while True:
+    resp = client.chat.completions.create(
+        model="chatgpt-4o-latest",
+        messages=[{"role": "user", "content": str(input())}]
+    )
+
+    print(resp.choices[0].message.content)
+
