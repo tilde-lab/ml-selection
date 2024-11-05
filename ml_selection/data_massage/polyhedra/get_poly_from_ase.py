d134a2537a488f20e252b87158cda4378f1b1282
alinzh <siberiangodness@mail.ru>
2024-11-05 13:20:22 +0000

    Add PolyhedronIdentifier class

--- ml_selection/data_massage/polyhedra/get_poly_from_ase.py
+++ ml_selection/data_massage/polyhedra/get_poly_from_ase.py
@@ -3,6 +3,7 @@ from os.path import isfile, join
 
 import numpy as np
 from ase.neighborlist import NeighborList
+from scipy.spatial import distance_matrix, ConvexHull
 
 from metis_backend.datasources.fmt import detect_format
 from metis_backend.structures.cif_utils import cif_to_ase
@@ -20,6 +21,215 @@ radius = {
     "trigonal": 0.8,
 }
 
+
+class PolyhedronIdentifier:
+    def __init__(self, points):
+        self.points = np.array(points)
+        self.distances = distance_matrix(self.points, self.points)
+        
+        self.polyhedra_properties = {
+            # from 1 to 3 vertix edges=0, cos unavailable to bilde ConvexHull object (define by num vertices)
+            "1#a": {"vertices": 1, "edges": 0, "angles": []},
+            "2#a": {"vertices": 2, "edges": 0, "angles": []},
+            "2#b": {"vertices": 2, "edges": 0, "angles": []},
+            "3#a": {"vertices": 3, "edges": 0, "angles": []},
+            "3#b": {"vertices": 3, "edges": 0, "angles": []},
+            
+            "4-a": {"vertices": 4, "edges": 6, "angles": [60]},
+            "4#b": {"vertices": 4, "edges": 6, "angles": [60]},  # tetrahedron; central atom outside
+            "4#c": {"vertices": 4, "edges": 4, "angles": [90]},  # coplanar square
+            "4#d": {"vertices": 4, "edges": 4, "angles": [90]},  # non-coplanar square
+            "5-a": {"vertices": 5, "edges": 8, "angles": [90]},  # square pyramid
+            "5-c": {"vertices": 5, "edges": 8, "angles": [60, 90]},  # trigonal bipyramid
+            "5#d": {"vertices": 5, "edges": 8, "angles": [90]},  # square pyramid; central atom outside
+            "5#e": {"vertices": 5, "edges": 5, "angles": [108]},  # coplanar pentagon
+            "6-a": {"vertices": 6, "edges": 12, "angles": [60, 90]},  # octahedron
+            "6-b": {"vertices": 6, "edges": 9, "angles": [90]},  # trigonal prism
+            "6-d": {"vertices": 6, "edges": 10, "angles": [108]},  # pentagonal pyramid
+            "6-h": {"vertices": 6, "edges": 9, "angles": [60, 90]},  # distorted prism
+            "6#c": {"vertices": 6, "edges": 6, "angles": [120]},  # coplanar hexagon
+            "6#e": {"vertices": 6, "edges": 6, "angles": [120]},  # non-coplanar hexagon
+            "7-a": {"vertices": 7, "edges": 15, "angles": [60, 90]},  # monocapped octahedron
+            "7-c": {"vertices": 7, "edges": 12, "angles": [90]},  # top-monocapped trigonal prism
+            "7-d": {"vertices": 7, "edges": 7, "angles": [120]},  # bicapped hexagon
+            "7-e": {"vertices": 7, "edges": 10, "angles": [120]},  # hexagonal pyramid
+            "7-g": {"vertices": 7, "edges": 12, "angles": [90]},  # monocapped trigonal prism
+            "7-h": {"vertices": 7, "edges": 10, "angles": [108]},  # pentagonal bipyramid
+            "8-a": {"vertices": 8, "edges": 12, "angles": [90]},  # square prism; cube
+            "8-b": {"vertices": 8, "edges": 12, "angles": [70, 110]},  # square antiprism
+            "8-c": {"vertices": 8, "edges": 12, "angles": [60]},  # hexagonal bipyramid
+            "8-d": {"vertices": 8, "edges": 12, "angles": [70, 110]},  # distorted square anti-prism-a
+            "8-e": {"vertices": 8, "edges": 12, "angles": [90]},  # 8-vertex
+            "8-g": {"vertices": 8, "edges": 12, "angles": [60, 90]},  # double anti-trigonal prism
+            "8-i": {"vertices": 8, "edges": 12, "angles": [90]},  # side-bicapped trigonal prism
+            "8-j": {"vertices": 8, "edges": 12, "angles": [70, 110]},  # distorted square anti-prism-b
+            "9-a": {"vertices": 9, "edges": 15, "angles": [90]},  # tricapped trigonal prism
+            "9-b": {"vertices": 9, "edges": 15, "angles": [90]},  # monocapped square prism
+            "9-c": {"vertices": 9, "edges": 15, "angles": [90]},  # monocapped double anti-trigonal prism
+            "9-d": {"vertices": 9, "edges": 16, "angles": [120]},  # truncated hexagonal pyramid
+            "9-e": {"vertices": 9, "edges": 14, "angles": [120]},  # bicapped hexagonal pyramid
+            "10-a": {"vertices": 10, "edges": 18, "angles": [90]},  # 4-capped trigonal prism
+            "10-b": {"vertices": 10, "edges": 17, "angles": [90, 51, 60, 77]},  # bicapped square prism
+            "10-c": {"vertices": 10, "edges": 17, "angles": [70, 110]},  # bicapped square antiprism
+            "10-d": {"vertices": 10, "edges": 20, "angles": [72, 120]},  # triangle-pentagon faced polyhedron
+            "10-e": {"vertices": 10, "edges": 18, "angles": [70, 110]},  # distorted equatorial 4-capped trigonal prism
+            "10-h": {"vertices": 10, "edges": 18, "angles": [120]},  # truncated hexagonal bipyramid
+            "10-j": {"vertices": 10, "edges": 18, "angles": [90]},  # polar bicapped square prism
+            "10-m": {"vertices": 10, "edges": 15, "angles": [108]},  # pentagonal prism
+            "10-o": {"vertices": 10, "edges": 15, "angles": []},  # 10-vertex
+            "10-s": {"vertices": 10, "edges": 18, "angles": [60, 120]},  # octagonal bipyramid
+            "11-a": {"vertices": 11, "edges": 20, "angles": [90]},  # 5-capped trigonal prism
+            "11-b": {"vertices": 11, "edges": 22, "angles": [60, 90]},  # pseudo Frank-Kasper
+            "11-c": {"vertices": 11, "edges": 21, "angles": [60, 120]},  # monotruncated icosahedron
+            "11-d": {"vertices": 11, "edges": 21, "angles": [90]},  # tricapped cube; same plane
+            "11-e": {"vertices": 11, "edges": 21, "angles": [90]},  # tricapped cube; different plane
+            "12-a": {"vertices": 12, "edges": 30, "angles": [60, 120]},  # icosahedron
+            "12-b": {"vertices": 12, "edges": 24, "angles": [90]},  # cuboctahedron
+            "12-c": {"vertices": 12, "edges": 19, "angles": [108]},  # bicapped pentagonal prism
+            "12-d": {"vertices": 12, "edges": 24, "angles": [60, 90]},  # anticuboctahedron
+            "12-f": {"vertices": 12, "edges": 18, "angles": [120]},  # hexagonal prism
+            "12-o": {"vertices": 12, "edges": 30, "angles": [72, 120]},  # triangle-hexagon faced polyhedron
+            "12-q": {"vertices": 12, "edges": 24, "angles": [90]},  # double-bicapped cube
+            "13-a": {"vertices": 13, "edges": 24, "angles": [60, 90]},  # pseudo Frank-Kasper
+            "13-b": {"vertices": 13, "edges": 22, "angles": [108]},  # tricapped pentagonal prism
+            "13-d": {"vertices": 13, "edges": 26, "angles": [60, 90]},  # monocapped cuboctahedron-a
+            "13-g": {"vertices": 13, "edges": 26, "angles": [60, 90]},  # monocapped cuboctahedron-b
+            "14-a": {"vertices": 14, "edges": 24, "angles": [60, 90]},  # 14-vertex Frank-Kasper
+            "14-b": {"vertices": 14, "edges": 24, "angles": [60, 90]},  # rhombic dodecahedron
+            "14-c": {"vertices": 14, "edges": 24, "angles": [60, 90]},  # distorted rhombic dodecahedron
+            "14-d": {"vertices": 14, "edges": 25, "angles": [108]},  # bicapped hexagonal prism
+            "14-e": {"vertices": 14, "edges": 30, "angles": [120]},  # 6-capped hexagonal bipyramid
+            "14-m": {"vertices": 14, "edges": 28, "angles": [108]},  # double-bicapped hexagonal prism
+            "15-a": {"vertices": 15, "edges": 26, "angles": [60, 90]},  # 15-vertex Frank-Kasper
+            "15-d": {"vertices": 15, "edges": 27, "angles": [72, 108]},  # equatorial 5-capped pentagonal prism
+            "15-e": {"vertices": 15, "edges": 27, "angles": [72, 108]},  # polar equatorial tricapped pentagonal prism
+            "15-f": {"vertices": 15, "edges": 27, "angles": [60, 90]},  # 15-vertex
+            "15-i": {"vertices": 15, "edges": 30, "angles": [60, 120]},  # monotruncated 16-vertex Frank-Kasper
+            "16-a": {"vertices": 16, "edges": 30, "angles": [60, 90]},  # 16-vertex Frank-Kasper
+            "16-b": {"vertices": 16, "edges": 32, "angles": [60, 90]},  # 16-vertex
+            "16-h": {"vertices": 16, "edges": 33, "angles": [90]},  # defective 8-equatorial capped pentagonal prism
+            "16-r": {"vertices": 16, "edges": 24, "angles": [90]},  # octagonal prism
+            "17-d": {"vertices": 17, "edges": 33, "angles": [72, 108]},  # 7-capped pentagonal prism
+            "18-a": {"vertices": 18, "edges": 35, "angles": [72, 108]},  # 8-equatorial capped pentagonal prism
+            "18-c": {"vertices": 18, "edges": 36, "angles": [60, 90]},  # pseudo Frank-Kasper
+            "18-d": {"vertices": 18, "edges": 36, "angles": [90, 120]},  # 6-capped hexagonal prism
+            "18-f": {"vertices": 18, "edges": 36, "angles": [108]},  # double-bicapped heptagonal prism
+            "19-b": {"vertices": 19, "edges": 40, "angles": [60, 90]},  # distorted pseudo Frank-Kasper
+            "19-c": {"vertices": 19, "edges": 38, "angles": [60, 90]},  # pseudo Frank-Kasper
+            "20-a": {"vertices": 20, "edges": 30, "angles": [60, 120]},  # pseudo Frank-Kasper
+            "20-h": {"vertices": 20, "edges": 40, "angles": [108]},  # 20-pentagonal faced polyhedron
+            "21-b": {"vertices": 21, "edges": 42, "angles": [60, 90]},  # pseudo Frank-Kasper
+            "22-a": {"vertices": 22, "edges": 44, "angles": [90, 120]},  # polar 8-equatorial capped hexagonal prism
+            "23-a": {"vertices": 23, "edges": 46, "angles": [60, 90]},  # pseudo Frank-Kasper
+            "24-a": {"vertices": 24, "edges": 36, "angles": [90, 120]},  # 6-square; 8-hexagonal faced polyhedron
+            "24-b": {"vertices": 24, "edges": 48, "angles": [60, 90]},  # pseudo Frank-Kasper
+            "24-c": {"vertices": 24, "edges": 36, "angles": [90]},  # truncated cube; dice
+            "24-d": {"vertices": 24, "edges": 36, "angles": [90, 120]},  # triangle-square-hexagon faced polyhedron
+            "24-g": {"vertices": 24, "edges": 36, "angles": [90]},  # triangle-square faced polyhedron
+            "26-a": {"vertices": 26, "edges": 52, "angles": [60, 90]},  # pseudo Frank-Kasper
+            "28-a": {"vertices": 28, "edges": 56, "angles": [60, 90]},  # pseudo Frank-Kasper
+            "32-a": {"vertices": 32, "edges": 60, "angles": [60, 90]},  # 32-vertex
+        }
+    
+    def classify(self):
+        num_vertices = len(self.points)
+        unique_edges = self._get_unique_edges()
+        num_edges = len(unique_edges)
+        angles = self._calculate_face_angles()
+
+        closest_match = None
+        min_difference = float('inf')
+        
+        # look for an exact match by the number of vertices
+        for poly_name, properties in self.polyhedra_properties.items():
+            if properties["vertices"] != num_vertices:
+                continue  
+
+            edge_diff = abs(properties["edges"] - num_edges)
+            angle_diff = self._compare_angles(properties["angles"], angles)
+
+            difference = edge_diff + angle_diff
+
+            if difference < min_difference:
+                min_difference = difference
+                closest_match = poly_name
+
+        # if an exact match is found by vertices and other properties, return the result
+        if closest_match:
+            return f"Closest match with same number of vertices: {closest_match} (difference score: {min_difference})"
+        
+        # if no exact match by vertices is found, try to find a match by other properties
+        for poly_name, properties in self.polyhedra_properties.items():
+            vertex_diff = abs(properties["vertices"] - num_vertices)
+            edge_diff = abs(properties["edges"] - num_edges)
+            angle_diff = self._compare_angles(properties["angles"], angles)
+
+            difference = vertex_diff + edge_diff + angle_diff
+
+            if difference < min_difference:
+                min_difference = difference
+                closest_match = poly_name
+
+        return f"No exact match. Closest polyhedron: {closest_match} (difference score: {min_difference})"
+    
+    def _get_unique_edges(self):
+        """Define unique edges using ConvexHull."""
+        # ConvexHull requires at min 4 points !!!
+        if len(self.points) < 4:
+            return []
+
+        hull = ConvexHull(self.points)
+        
+        edges = set()
+        
+        for obj in hull.simplices:
+            for i in range(len(obj)):
+                for j in range(i + 1, len(obj)):
+                    # store each pair of point indices in sorted order to avoid duplicates
+                    edge = tuple(sorted((obj[i], obj[j])))
+                    edges.add(edge)
+        
+        # calculate lengths of unique edges
+        unique_edges = np.unique([self.distances[edge] for edge in edges])
+        
+        return unique_edges
+    
+    def _calculate_face_angles(self):
+        """Calculates angles between polyhedron edges based on its convex hull."""
+        # сheck if there are enough points to build the hull
+        if len(self.points) < 4:
+            return []
+
+        # сreate the convex hull of the polyhedron
+        hull = ConvexHull(self.points)
+        angles = []  # List to store all angles between edges
+
+        # iter over each face of the convex hull
+        for simplex in hull.simplices:
+            for i in range(len(simplex)):
+                p1 = self.points[simplex[i]]
+                p2 = self.points[simplex[(i + 1) % len(simplex)]]  # next vertex
+                p3 = self.points[simplex[(i + 2) % len(simplex)]]  # vertex after next
+
+                vec1 = p2 - p1
+                vec2 = p3 - p1
+                cosine_angle = np.dot(vec1, vec2) / (np.linalg.norm(vec1) * np.linalg.norm(vec2))
+                angle = np.degrees(np.arccos(np.clip(cosine_angle, -1.0, 1.0)))
+
+                angles.append(angle)
+        
+        # remove duplicate
+        unique_angles = np.unique(angles)
+        return unique_angles.tolist()
+    
+    def _compare_angles(self, known_angles, detected_angles):
+        """Compares the angles of the detected polyhedron with known angles for this type."""
+        angle_diff = 0
+        for known in known_angles:
+            closest_angle = min(detected_angles, key=lambda x: abs(x - known))
+            angle_diff += abs(closest_angle - known)
+        return angle_diff
+    
 
 def sg_to_crystal_system(num: int):
     if 195 <= num <= 230:
@@ -58,12 +268,10 @@ def extract_poly(crystal_obj=None, cutoff=None) -> list[dict]:
     polyhedrons : list[dict]
         List of dictionaries containing center atom, atoms of poly, distance
     """
+    lattice_type = sg_to_crystal_system(crystal_obj.info["spacegroup"].no)
+
     # atomic cut-off radius
-    cutoff = radius[sg_to_crystal_system(crystal_obj.info["spacegroup"].no)]
-
-    if sg_to_crystal_system(crystal_obj.info["spacegroup"].no) != "cubic":
-        return None
-
+    cutoff = radius[lattice_type]
     cutoffs = [cutoff] * len(crystal_obj)
 
     # make list with all neighbors in current cut-off
@@ -91,16 +299,29 @@ def extract_poly(crystal_obj=None, cutoff=None) -> list[dict]:
 
             if (center_atom, comp) not in polyhedrons:
                 print(center_atom, comp)
+                
+                # poly_coords = [crystal_obj.positions[i]]  # start with central atom's position
+                poly_coords = []
+                poly_coords.extend(neighbor_coords)       # Add all neighbor positions
 
-                type_poly = sg_to_crystal_system(crystal_obj.info["spacegroup"].no)
-                print(f"Type pf poly is: {type_poly}")
+                lattice = sg_to_crystal_system(crystal_obj.info["spacegroup"].no)
+                print(f"Type оf lattice is: {lattice}")
 
-                polyhedrons.append(comp)
+                polyhedrons.append((center_atom, comp))
                 polyhedron = {
                     "center_atom": center_atom,
                     "neighbors": comp,
-                    "distances": [0.0] + distances.tolist(),  # 0.0 for central atom
+                    "distances": [0.0] + distances.tolist()  # 0.0 for central atom
                 }
+                
+                classifier = PolyhedronIdentifier(poly_coords)
+                
+                try:
+                    polyhedron_type = classifier.classify()
+                except:
+                    polyhedron_type = "unknown"
+                print(polyhedron_type)
+                print('Defined type of polyhedron №', len(polyhedrons))
 
     return polyhedrons
 
@@ -148,12 +369,9 @@ def get_polyhedrons(path_structures: str = None, structures: str = None) -> list
 
 if __name__ == "__main__":
     onlyfiles = [
-        f for f in listdir("cif/trigonal/") if isfile(join("cif/trigonal/", f))
+        f for f in listdir("ml_selection/structures_props/cif") if isfile(join("ml_selection/structures_props/cif", f))
     ]
 
     for file in onlyfiles:
         print(file)
-        try:
-            get_polyhedrons("cif/trigonal/" + file)
-        except:
-            continue
+        get_polyhedrons("ml_selection/structures_props/cif/" + file)
