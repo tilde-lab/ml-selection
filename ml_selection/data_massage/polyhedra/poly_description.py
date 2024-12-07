a3c0522df6a0fba769a8fee6623a344941bd3c4b
alinzh <siberiangodness@mail.ru>
2024-12-07 17:27:27 +0000

    Update mapping with AETs

--- ml_selection/data_massage/polyhedra/poly_description.py
+++ ml_selection/data_massage/polyhedra/poly_description.py
@@ -80,12 +80,12 @@ polyhedra_properties = {
             "14-f": {"vertices": 14, "aet": [{'6': {'3': 3, '4': 1}}, {'3': {'3': 5}}, {'2': {'3': 7}}, {'2': {'3': 5, '4': 1}}, {'1': {'3': 3}}]},
             "14-g": {"vertices": 14, "aet": [{'6': {'3': 3, '5': 1}}, {'4': {'3': 2, '4': 1, '5': 1}}, {'2': {'3': 6}}, {'2': {'3': 5}}]},  
             "15-a": {"vertices": 15, "aet": [{'12': {'3': 5}}, {'3': {'3': 6}}]},
-            # "15-d": {"vertices": 15, "edges": 47, "polygon": [72, 108]},  # equatorial 5-capped pentagonal prism
+            # "15-d": {"vertices": 15, },  # equatorial 5-capped pentagonal prism
             "15-c": {"vertices": 15, "aet": [{'8': {'3': 6}}, {'5': {'3': 4}}, {'2': {'3': 5}}]},  
             "15-d": {"vertices": 15, "aet": [{'6': {'3': 3, '5': 1}}, {'4': {'3': 4, '5': 1}}, {'2': {'3': 6}}, {'2': {'3': 5}}, {'1': {'3': 4}}]}, 
             "15-e": {"vertices": 15, "aet": [{'4': {'3': 7}}, {'2': {'3': 5}}, {'2': {'3': 4, '4': 1}}, {'2': {'3': 3, '4': 1}}, {'2': {'3': 3}}, {'2': {'3': 2, '4': 2}}, {'1': {'3': 4}}]}, 
             "16-a": {"vertices": 16, "aet": [{'12': {'3': 5}}, {'4': {'3': 6}}]},
-            # "16-b": {"vertices": 16, "edges": 52, "polygon": [60, 90]},  # 16-vertex
+            # "16-b": {"vertices": 16, },  # 16-vertex
             "16-c": {"vertices": 16, "aet": [{'9': {'3': 6}}, {'3': {'3': 4}}, {'3': {'3': 3}}, {'1': {'3': 9}}]},  
             "16-d": {"vertices": 16, "aet": [{'6': {'3': 4, '5': 1}}, {'3': {'3': 6}}, {'3': {'3': 4}}, {'3': {'3': 1, '5': 2}}, {'1': {'5': 3}}]},  
             "17-a": {"vertices": 17, "aet": [{'10': {'3': 5}}, {'4': {'3': 4, '4': 1}}, {'3': {'3': 6}}]},  
@@ -93,7 +93,6 @@ polyhedra_properties = {
             "17-d": {"vertices": 17, "aet": [{'12': {'3': 5}}, {'5': {'3': 6}}]},  # 7-capped pentagonal prism
             "17-e": {"vertices": 17, "aet": [{'6': {'3': 6}}, {'4': {'3': 3}}, {'3': {'3': 7}}, {'3': {'3': 4}}, {'1': {'3': 9}}]},
             "17-f": {"vertices": 17, "aet": [{'12': {'3': 4, '4': 1}}, {'3': {'3': 4}}, {'2': {'3': 6}}]},
-            
             "18-a": {"vertices": 18, "aet": [{'12': {'3': 6}}, {'5': {'3': 4}}]},  # !8-equatorial capped pentagonal prism
             "18-c": {"vertices": 18, "aet": [{'12': {'3': 5}}, {'6': {'3': 6}}]},  # pseudo Frank-Kasper
             "18-d": {"vertices": 18, "aet": [{'12': {'3': 2, '4': 1, '6': 1}}, {'6': {'3': 2, '4': 2}}]},  # 6-capped hexagonal prism
@@ -101,18 +100,18 @@ polyhedra_properties = {
             "19-b": {"vertices": 19, "aet": [{'15': {'3': 5}}, {'3': {'3': 7}}, {'1': {'3': 6}}]},  # distorted pseudo Frank-Kasper
             "19-c": {"vertices": 19, "aet": [{'9': {'3': 4, '4': 1}}, {'3': {'3': 5, '4': 1}}, {'3': {'3': 5}}, {'3': {'3': 4}}, {'1': {'3': 6}}]},  # pseudo Frank-Kasper
             "20-a": {"vertices": 20, "aet": [{'12': {'3': 5}}, {'8': {'3': 8}}]},
-            # "20-h": {"vertices": 20, "edges": 90, "polygon": [108]},  # 20-pentagonal faced polyhedron
+            # "20-h": {"vertices": 20, },  # 20-pentagonal faced polyhedron
             "21-a": {"vertices": 21, "aet": [{'4': {'3': 3, '4': 2}}, {'4': {'3': 2, '4': 2}}, {'3': {'3': 5}}, {'2': {'3': 5, '4': 1}}, {'2': {'3': 4, '4': 1}}, {'2': {'3': 3, '4': 1}}, {'1': {'3': 6, '4': 1}},
                                              {'1': {'3': 4, '4': 2}}, {'1': {'3': 2, '4': 1}},  {'1': {'3': 1, '4': 2}}]}, # pseudo Frank-Kasper
             "22-a": {"vertices": 22, "aet": [{'16': {'3': 2, '4': 2}}, {'4': {'3': 4, '4': 2}}, {'2': {'4': 1}}]},   # polar 8-equatorial capped hexagonal prism
             "23-a": {"vertices": 23, "aet": [{'14': {'3': 6}}, {'6': {'3': 5}}, {'3': {'3': 4}}]},  # pseudo Frank-Kasper
-            # "24-a": {"vertices": 24, "edges": 72, "polygon": [90, 120]},  # 6-square; 8-hexagonal faced polyhedron
-            # "24-b": {"vertices": 24, "edges": 72, "polygon": [60, 90]},  # pseudo Frank-Kasper
-            # "24-c": {"vertices": 24, "edges": 36, "polygon": [90]},  # truncated cube; dice
-            # "24-d": {"vertices": 24, "edges": 72, "polygon": [90, 120]},  # triangle-square-hexagon faced polyhedron
-            # "24-g": {"vertices": 24, "edges": 37, "polygon": [90]},  # triangle-square faced polyhedron
-            # "26-a": {"vertices": 26, "edges": 78, "polygon": [60, 90]},  # $ pseudo Frank-Kasper
-            # "28-a": {"vertices": 28, "edges": 84, "polygon": [60, 90]},  # $ pseudo Frank-Kasper
+            # "24-a": {"vertices": 24, },  # 6-square; 8-hexagonal faced polyhedron
+            # "24-b": {"vertices": 24, },  # pseudo Frank-Kasper
+            # "24-c": {"vertices": 24, },  # truncated cube; dice
+            # "24-d": {"vertices": 24, },  # triangle-square-hexagon faced polyhedron
+            # "24-g": {"vertices": 24, },  # triangle-square faced polyhedron
+            # "26-a": {"vertices": 26, },  # $ pseudo Frank-Kasper
+            # "28-a": {"vertices": 28, },  # $ pseudo Frank-Kasper
             "30-a": {"vertices": 30, "aet": [{'18': {'3': 4, '4': 1}}, {'6': {'3': 5, '4': 1}}, {'6': {'3': 5}}]}, 
-            # "32-a": {"vertices": 32, "edges": 96, "polygon": [60, 90]},  # $ 32-vertex
+            # "32-a": {"vertices": 32, },  # $ 32-vertex
         }
